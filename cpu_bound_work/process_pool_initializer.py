e597805f341d43d24e76ef171c6d873fc2c02a7e
bosung park <81157873+bosungpark@users.noreply.github.com>
2022-10-17 00:23:04 +0900

    Feat: pool initializer

---
+++ cpu_bound_work/process_pool_initializer.py
@@ -0,0 +1,22 @@
+import asyncio
+from concurrent.futures import ProcessPoolExecutor
+from multiprocessing import Value, Process
+
+shared_counter: Value
+
+def init(counter: Value):
+    global shared_counter
+    shared_counter =counter
+
+def increment():
+    with shared_counter.get_lock():
+        shared_counter.value +=1
+
+async def main():
+    counter = Value("d",0) # shared
+    with ProcessPoolExecutor(initializer=init,
+                             initargs=(counter,)) as pool:
+        await asyncio.get_running_loop().run_in_executor(pool, increment)
+
+if __name__ == '__main__':
+    asyncio.run(main())
